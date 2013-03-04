a4bbe422b8b1d519f1ce7453ae7c4c584fd1f7b3
Imdsm <adamkdean@googlemail.com>
2013-03-04 14:00:44 +0000

    Added render time in pi at bottom of page

--- PersonalWebsite/Global.asax.cs
+++ PersonalWebsite/Global.asax.cs
@@ -2,6 +2,7 @@
 using EasyAuth.Storage;
 using PersonalWebsite.Models;
 using System;
+using System.Diagnostics;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -12,10 +13,12 @@ namespace PersonalWebsite
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        // EasyAuth user storage. Entity is good, we'll use that!
         static IUserStore UserStore = EntityUserStore.Instance;
 
         protected void Application_Start()
         {
+            // setup EasyAuth
             EntityUserStore.Instance.ContextType = typeof(WebsiteContext);
             Authentication.UserStore = UserStore;
 
@@ -28,7 +31,18 @@ namespace PersonalWebsite
 
         protected void Application_BeginRequest(Object sender, EventArgs e)
         {
+            // pass this to EasyAuth so that it can use cookies etc.
             Authentication.HttpContext = HttpContext.Current;
+
+            // timing for the `page rendered` section
+            var stopwatch = new Stopwatch();
+            HttpContext.Current.Items["Stopwatch"] = stopwatch;
+            stopwatch.Start();
+        }
+
+        protected void Application_EndRequest(Object sender, EventArgs e)
+        {
+            //
         }
     }
 }
