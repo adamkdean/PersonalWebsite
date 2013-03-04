324f65922dd17f8aeac0c16ae237c6cba87a1ce2
Imdsm <adamkdean@googlemail.com>
2013-03-04 14:12:08 +0000

    Used T4MVC lowercase urls. Fixed navigation bar active selector

--- PersonalWebsite/App_Start/RouteConfig.cs
+++ PersonalWebsite/App_Start/RouteConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web.Mvc;
 using System.Web.Routing;
+using T4MVC;
 
 namespace PersonalWebsite
 {
@@ -8,7 +9,8 @@ namespace PersonalWebsite
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            
+            routes.LowercaseUrls = true;
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}/{slug}",
